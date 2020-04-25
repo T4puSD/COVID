171723b6b84bfd8974d1dc273783664dae79c734
T4puSD <sdtapusd20@gmail.com>
2020-04-25 20:17:17 +0600

    changed total cases to daily cases with plotly bar

--- js/plotit.js
+++ js/plotit.js
@@ -4,7 +4,7 @@ console.log("width : "+window_width);
 console.log("width perc: "+window_width*0.2);
 
 var confirmed_layout = {
-    title: 'Confirmed Cases',
+    title: 'Daily New Cases',
     autosize:false,
     height:window_width-(window_height*0.4),
     width:window_width,
@@ -13,20 +13,22 @@ var confirmed_layout = {
     },
     showlegend: false,
     xaxis: {
-      tickangle: -45
+      tickangle: -45,
+      tickmode: 'array'
     },
     yaxis: {
+      title: 'Daily Cases',
       zeroline: false,
       gridwidth: 2
     },
-    bargap :0.05,
+    bargap :0.5,
     autosize:true
   };
   // deep coping with spread syntax
   var recovered_layout = {...confirmed_layout};
-  recovered_layout.title = "Recovered cases";
+  recovered_layout.title = "Daily New Recovered";
   var deaths_layout = {...confirmed_layout}
-  deaths_layout.title = "Deaths";
+  deaths_layout.title = " Daily Deaths";
 
   var config = {
     displayModeBar: false,
@@ -49,30 +51,43 @@ var confirmed_layout = {
           let deaths_y_axis = [];
           let last30Days = jsonData[countryname].slice(-30,);
           // console.log(last30Days);
+          let prev_confirm = last30Days[0].confirmed;
+          let prev_recovered = last30Days[0].recovered;
+          let prev_deaths = last30Days[0].deaths;
+          // confirmed_y_axis.push(prev_confirm);
+          last30Days.map(element=>{
+            confirmed_y_axis.push(element.confirmed - prev_confirm);
+            recovered_y_axis.push(element.recovered - prev_recovered);
+            deaths_y_axis.push(element.deaths - prev_deaths);
+
+            prev_confirm = element.confirmed;
+            prev_recovered = element.recovered;
+            prev_deaths = element.deaths;
+          });
           last30Days.forEach(element => {
             // only plotting 30 days data
             // console.log(element);
               x_axis_dates.push(element.date);
-              confirmed_y_axis.push(element.confirmed);
-              recovered_y_axis.push(element.recovered);
+              // confirmed_y_axis.push(element.confirmed);
+              // recovered_y_axis.push(element.recovered);
               deaths_y_axis.push(element.deaths);
               
           });
           // console.log(confirmed_y_axis);
            let confirmed_data = [
               {
-              x: x_axis_dates,
-              y: confirmed_y_axis,
-              type: 'scatter',
+              x: x_axis_dates.slice(1,),
+              y: confirmed_y_axis.slice(1,),
+              type: 'bar',
               marker: {color: 'rgb(231, 237, 40)'}
               }
           ];
 
           let recovered_data = [
             {
-              x: x_axis_dates,
-              y: recovered_y_axis,
-              type: 'scatter',
+              x: x_axis_dates.slice(1,),
+              y: recovered_y_axis.slice(1,),
+              type: 'bar',
               marker: {color: 'rgb(38, 230, 34)'}
             }
           ];
@@ -80,7 +95,7 @@ var confirmed_layout = {
             {
               x: x_axis_dates,
               y: deaths_y_axis,
-              type: 'scatter',
+              type: 'bar',
               marker: {color: 'rgb(230, 53, 37)'}
             }
           ];
